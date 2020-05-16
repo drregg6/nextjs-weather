3a27d102140a71ec5984deecbc5a1be85e749ef5
Dave Regg <drregg6@gmail.com>
2020-05-15 20:06:55 -0400

    Layout significantly c changed in all Components

--- components/weather/hourly.js
+++ components/weather/hourly.js
@@ -7,55 +7,32 @@ In order to generate hourly into two rows
 
 import Moment from 'react-moment';
 import hourlyStyles from './hourly.module.scss';
+import Link from 'next/link';
 
 import { getIcon } from '../../utils/weatherHelper';
 import { capitalize } from '../../utils/stringHelper';
 
 const Hourly = ({ hourly, datetime }) => {
-  console.log(hourly)
-  const firstSix = hourly.slice(0,6);
-  const lastSix = hourly.slice(6);
+  console.log(hourly);
   return (
     <div className={hourlyStyles.container}>
       <h1 className={hourlyStyles.subtitle}>Twelve hour forecast</h1>
       <div className={hourlyStyles.forecast}>
-        <div className={hourlyStyles.row}>
         {
-          firstSix.map((hour, idx) => {
+          hourly.slice(0,6).map((hour, idx) => {
             return (
               <div key={idx} className={hourlyStyles.hour}>
-                <small><Moment add={{ hours: `${idx}` }} format="H:mm">{datetime}</Moment></small>
-                <img src={getIcon(hour.weather[0].icon)} alt={hour.weather[0].description} />
-                <div>
+                <h2 className={hourlyStyles.time}><Moment add={{ hours: `${idx}` }} format="H:mm">{datetime}</Moment></h2>
+                <div className={hourlyStyles.details}>
+                  <img src={getIcon(hour.weather[0].icon)} alt={hour.weather[0].description} />
                   <p>Temp: {Math.floor(hour.temp)}&deg;</p>
-                  <small>Feels like: {Math.floor(hour.feels_like)}&deg;</small>
-                </div>
-                <div>
-                  <p>{`${capitalize(hour.weather[0].description)}`}</p>
                 </div>
               </div>
             )
           })
         }
-        </div>
-        <div className={hourlyStyles.row}>
-        {
-          lastSix.map((hour, idx) => {
-            return (
-              <div key={idx} className={hourlyStyles.hour}>
-                <small><Moment add={{ hours: `${idx}` }} format="H:mm">{datetime}</Moment></small>
-                <img src={getIcon(hour.weather[0].icon)} alt={hour.weather[0].description} />
-                <div>
-                  <p>Temp: {Math.floor(hour.temp)}&deg;</p>
-                  <small>Feels like: {Math.floor(hour.feels_like)}&deg;</small>
-                </div>
-                <div>
-                  <p>{`${capitalize(hour.weather[0].description)}`}</p>
-                </div>
-              </div>
-            )
-          })
-        }
+        <div className={hourlyStyles.link}>
+          <Link href="#"><a>See further into the day...</a></Link>
         </div>
       </div>
     </div>
